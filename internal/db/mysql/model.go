f2f9e3f57c2c66e9af9dfe65d891a6960b65e2d5
Albert <nomorezzz@163.com>
2024-04-29 21:36:19 +0800

    add: 新增邮件通知服务

--- internal/db/mysql/model.go
+++ internal/db/mysql/model.go
@@ -1,25 +1,41 @@
 package mysql
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
-type Admin struct {
-	gorm.Model
-	username string
-	password string
-	role     string
-}
-
+// Commodity 映射商品表
 type Commodity struct {
 	gorm.Model
 	name        string
 	description string
 	price       float64
 	owner       string
+	img         string
 }
 
-type User struct {
+// Order 映射订单表
+type Order struct {
 	gorm.Model
-	name     string
-	password string
-	identity string
+	OrderID           uint      `gorm:"primaryKey;autoIncrement"`
+	CustomerID        uint      `gorm:"not null"`
+	OrderDate         time.Time `gorm:"not null"`
+	TotalAmount       float64   `gorm:"not null"`
+	Status            string    `gorm:"type:enum('pending','processing','shipped','completed','cancelled');default:'pending';not null"`
+	PaymentMethod     string    `gorm:"size:50"`
+	ShippingAddressID *uint
+	BillingAddressID  *uint
+}
+
+// OrderItem 订单商品关联表
+type OrderItem struct {
+	gorm.Model
+	ItemID    uint      `gorm:"primaryKey;autoIncrement"`
+	OrderId   uint      `gorm:"not null"`
+	ProductId uint      `gorm:"not null"`
+	Quantity  uint      `gorm:"not null"`
+	Price     float64   `gorm:"not null"`
+	Order     Order     `gorm:"foreignKey:OrderID"`
+	Product   Commodity `gorm:"foreignKey:ProductId"`
 }
