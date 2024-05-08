18f0cc72b05c5b22b84ed952605d6c1a93bd3cec
QAQ <m13154397827@163.com>
2024-05-08 17:44:07 +0800

    新增用户管理子系统

--- internal/router/user.go
+++ internal/router/user.go
@@ -16,15 +16,7 @@ func setUserRouter(r *gin.Engine) {
 		userGroup.GET("/profile/info", user.InquireInfo)
 		userGroup.GET("/profile/info/anyone_info", user.InquireAnyoneInfo)
 		// TODO 修改用户信息
-		userGroup.PUT("/profile/modify_username", user.ModifyUsername)
-		userGroup.PUT("/profile/modify_real_name", user.ModifyRealName)
-		userGroup.PUT("/profile/modify_password", user.ModifyPassword)
-		userGroup.PUT("/profile/modify_phone_number", user.ModifyPhoneNumber)
-		userGroup.PUT("/profile/modify_role", user.ModifyRole)
-		userGroup.PUT("/profile/modify_anyone_username", user.ModifyAnyoneUsername)
-		userGroup.PUT("/profile/modify_anyone_real_name", user.ModifyAnyoneRealName)
-		userGroup.PUT("/profile/modify_anyone_password", user.ModifyAnyonePassword)
-		userGroup.PUT("/profile/modify_anyone_phone_number", user.ModifyAnyonePhoneNumber)
-		userGroup.PUT("/profile/modify_anyone_role", user.ModifyAnyoneRole)
+		userGroup.PUT("/profile/modify_info", user.ModifyInfo)
+		userGroup.PUT("/profile/modify_anyone_role", user.ModifyAnyoneInfo)
 	}
 }
--- internal/service/operation/inquire.go
+++ internal/service/operation/inquire.go
@@ -22,159 +22,38 @@ func User(db *gorm.DB, username string) *mysql.User {
 	return &user
 }
 
-// UserRealName
-//
-//	@Description: 查询用户真实名字
-//	@param	db			数据库DB
-//	@param	username	查询用户真实名字
-//	@return	string		真实名字字符串
-func UserRealName(db *gorm.DB, username string) string {
-	user := User(db, username)
-	if user == nil {
-		return ""
-	}
-	return user.RealName
-}
-
-// UserPassword
-//
-//	@Description: 查询用户密码
-//	@param	db			数据库DB
-//	@param	username	查询用户名
-//	@return	string		密码字符串
-func UserPassword(db *gorm.DB, username string) string {
-	user := User(db, username)
-	if user == nil {
-		return ""
-	}
-	return user.Password
-}
-
-// UserPhoneNumber
-//
-//	@Description: 查询用户手机号
-//	@param	db			数据库DB
-//	@param	username	查询用户名
-//	@return	string		手机号字符串
-func UserPhoneNumber(db *gorm.DB, username string) string {
-	user := User(db, username)
-	if user == nil {
-		return ""
-	}
-	return user.PhoneNumber
-}
-
-// UserRoleID
-//
-//	@Description: 查询用户角色ID
-//	@param	db			数据库DB
-//	@param	username
-//	@return	uint
-func UserRoleID(db *gorm.DB, username string) uint {
-	user := User(db, username)
-	if user == nil {
-		return 0
-	}
-	return user.RoleID
-}
-
 // HasPermission
 //
 //	@Description: 查询用户是否具有权限
-//	@param	db				数据库DB
-//	@param	username		用户名
-//	@param	permissionName	权限字符串
-//	@return	bool			查询结果
-func HasPermission(db *gorm.DB, username string, permissionID uint) bool {
+//	@param	db			数据库DB
+//	@param	username	用户名
+//	@param	permission	权限字符串
+//	@return	bool		查询结果
+func HasPermission(db *gorm.DB, username string, permission string) bool {
 	user := User(db, username)
 	if user == nil {
 		return false
 	}
-	permissions := RolePermissionID(db, user.RoleID)
-	for _, permission := range permissions {
-		if permission == permissionID {
+	permissions := RolePermission(db, user.Role)
+	for _, p := range permissions {
+		if p == permission {
 			return true
 		}
 	}
 	return false
 }
 
-/*
-// RoleID
-//
-//	@Description: 查询角色ID
-//	@param	db			数据库DB
-//	@param	roleName	角色字符串
-//	@return	uint		角色ID
-func RoleID(db *gorm.DB, roleName string) uint {
-	roleName = strings.ToLower(roleName)
-	var role mysql.Role
-	result := db.Where("role_name = ?", roleName).First(&role)
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		return 0
-	}
-	return role.ID
-}
-
-// RoleName
-//
-//	@Description: 查询角色名
-//	@param	db		数据库DB
-//	@param	roleID	角色ID
-//	@return	string	角色字符串
-func RoleName(db *gorm.DB, roleID uint) string {
-	var role mysql.Role
-	result := db.First(&role, roleID)
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		return ""
-	}
-	return role.Name
-}
-*/
-
-// RolePermissionID
+// RolePermission
 //
 //	@Description: 查询角色具有的权限
-//	@param	db		数据库DB
-//	@param	roleID	角色ID
-//	@return	[]uint	权限切片
-func RolePermissionID(db *gorm.DB, roleID uint) []uint {
-	var permissionIDs []uint
-	db.Model(&mysql.RolePermission{}).Where("role_id = ?", roleID).Pluck("permission_id", &permissionIDs)
-	if len(permissionIDs) == 0 {
+//	@param	db			数据库DB
+//	@param	role		角色ID
+//	@return	[]string	权限切片
+func RolePermission(db *gorm.DB, role string) []string {
+	var permissions []string
+	db.Model(&mysql.RolePermission{}).Where("role = ?", role).Pluck("permission", &permissions)
+	if len(permissions) == 0 {
 		return nil
 	}
-	return permissionIDs
+	return permissions
 }
-
-/*
-// PermissionID
-//
-//	@Description: 查询权限ID
-//	@param	db				数据库DB
-//	@param	permissionName	权限字符串
-//	@return	uint			权限ID
-func PermissionID(db *gorm.DB, permissionName string) uint {
-	var permission mysql.Permission
-	result := db.Where("permission_name = ?", permissionName).First(&permission)
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		return 0
-	}
-	return permission.ID
-}
-
-// PermissionName
-//
-//	@Description: 查询权限名
-//	@param	db				数据库DB
-//	@param	permissionID	权限ID
-//	@return	string			权限字符串
-func PermissionName(db *gorm.DB, permissionID uint) string {
-	var permission mysql.Permission
-	result := db.First(&permission, permissionID)
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		return ""
-	}
-	return permission.Name
-}
-*/
